6d4bc0541eca476d96f3012240c98fb8be588e13
MridulSheikh <prince9nazir@gmail.com>
2022-04-08 01:24:33 +0600

    abou consumer

--- src/pages/about/AboutConsumer.jsx
+++ src/pages/about/AboutConsumer.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import Aboutbanner from '../../components/about/AboutBanner';
+import Aboutcobottom from '../../components/about/AboutCoBottom';
+import Aboutcodefference from '../../components/about/AboutCoDefference';
 import Aboutcofeturedind from '../../components/about/AboutCoFeturedInd';
+import Aboutcomap from '../../components/about/AboutCoMap';
 import Aboutcoourmission from '../../components/about/AboutCOourMission';
+import Aboutcotrustory from '../../components/about/AboutCoTrustory';
+import Aboutpower from '../../components/about/AboutPower';
+import Aboutsmarteragent from '../../components/about/AboutSmarterAgent';
 import Layout from '../../components/Layout';
 import Navigation from '../../components/Navigation';
 
@@ -13,6 +19,12 @@ const Aboutconsumer = () => {
                 <Aboutbanner />
                 <Aboutcoourmission />
                 <Aboutcofeturedind />
+                <Aboutsmarteragent />
+                <Aboutpower />
+                <Aboutcotrustory />
+                <Aboutcodefference />
+                <Aboutcomap />
+                <Aboutcobottom />
             </Container>
         </Layout>
     );
