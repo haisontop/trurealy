37a5170c26c386e5a43e9fc2046b0c06a6d13432
MridulSheikh <prince9nazir@gmail.com>
2022-04-09 02:19:03 +0600

    fix img

--- src/components/giving-back/GivingComapanyLogo.tsx
+++ src/components/giving-back/GivingComapanyLogo.tsx
@@ -5,10 +5,18 @@ function GivingComapanyLogo() {
     <div className='container mx-auto my-5 pt-5'>
          <p className='fs-4 text-center'>A few of the amazing organizations we have helped.</p>
          <div className='d-flex justify-content-around mt-4'>
-          <img src="/assets/logo/FMSC-White-RGB.png" alt="" />
-          <img src="/assets/logo/ahs_logo.png" alt="" />
-          <img src="/assets/logo/CFlogo.png" alt="" />
-          <img src="/assets/logo/top-logo-e1563213839709.png" alt="" />
+           <div>
+             <img src="/assets/logo/FMSC-White-RGB.png" className='my-auto' alt="" />
+           </div>
+           <div>
+             <img src="/assets/logo/ahs_logo.png" className='my-auto' alt="" />
+           </div>
+           <div>
+             <img src="/assets/logo/CFlogo.png" className='my-3' alt="" />
+           </div>
+           <div>
+             <img src="/assets/logo/top-logo-e1563213839709.png" className='my-auto' alt="" />
+           </div>
          </div>
     </div>
   )
