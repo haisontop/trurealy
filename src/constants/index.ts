6de49f836bdc1557a5a674a0d038dab80a41eb00
haisontop <haisontop@gmail.com>
2022-04-11 11:02:07 -0700

    remove codes

--- src/constants/index.ts
+++
@@ -1,5 +0,0 @@
-import { FAQ_DATA } from "./FAQ_MOCK_DATA";
-import { SMATER_DATA } from "./SMARTER_MOCK_DATA";
-import { trustory } from "./TRUSTORY-MOCK_DATA";
-
-export { FAQ_DATA, SMATER_DATA, trustory };
