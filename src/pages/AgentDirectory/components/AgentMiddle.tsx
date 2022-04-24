504c417895b36b649c2297f56774d055582f0324
MridulSheikh <prince9nazir@gmail.com>
2022-04-25 02:27:45 +0600

    agent directory

--- src/pages/AgentDirectory/components/AgentMiddle.tsx
+++ src/pages/AgentDirectory/components/AgentMiddle.tsx
@@ -7,13 +7,36 @@ import style from "./AgentMiddleCard/AgentMiddleCard.module.scss";
 function AgentMiddle() {
     const settings = {
         dots: true,
+        dotsClass: `slick-dots ${style["custom-dots"]}`,
         className: "center",
         centerMode: true,
-        infinite: true,
+        infinite: false,
         centerPadding: "0px",
         slidesToShow: 3,
         speed: 500,
         rows: 3,
+        appendDots: (dots: any) => (
+            <div
+              style={{
+                borderRadius: "10px",
+                padding: "10px",
+              }}
+            >
+              <ul> {dots} </ul>
+            </div>
+          ),
+          customPaging: (i: number) => (
+            <div
+              style={{
+                width: "30px",
+                color: "#292670",
+                border: "1px #292670 solid",
+              }}
+              className="gx-5 fs-3"
+            >
+              {i + 1}
+            </div>
+          )
       };
     return (
         <div className={`g-5 px-5 position-relative ${style["Agent-middle"]}`}>
