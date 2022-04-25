f2864c45c8a7c5536a9bb96b8d56fd78cef9d5d9
iwancergey <iwancergey1@gmail.com>
2022-04-25 20:47:03 +0800

    giving page top fix

--- src/pages/GivingBack/components/GivingBackBanner/GivingBackBanner.tsx
+++ src/pages/GivingBack/components/GivingBackBanner/GivingBackBanner.tsx
@@ -1,92 +1,27 @@
 import React from "react";
 import { GradientDivider } from "../../../../components/GradientDivider";
+import { BsSuitHeart } from 'react-icons/bs';
 
 function GivingBackcoBanner() {
     return (
         <>
-            <div
-                className="bnnr_section inner_bnnr_sec gve_bck_bnnr"
-                style={{
-                    background:
-                        "url('/img/gvng_bnnr.jpg') no-repeat center center",
-                    backgroundSize: "cover",
-                }}
-            >
-                <div className="bnnr_top_box">
-                    <div className="bnnr_txt">
-                        <h1 className="d-flex align-items-center">
-                            <span className="me-4">#TRUCARES</span>
+            <div className="position-relative">
+                <img src="/assets/bg/giving-bg.png" alt="" loading="lazy" />
 
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="5.25rem"
-                                viewBox="0 0 84.119 74.896"
-                            >
-                                <defs>
-                                    <clipPath id="clip-path">
-                                        <rect
-                                            id="Rectangle_369"
-                                            data-name="Rectangle 369"
-                                            width="84.119"
-                                            height="74.896"
-                                            fill="#fd084f"
-                                        />
-                                    </clipPath>
-                                </defs>
-                                <g
-                                    id="Group_93"
-                                    data-name="Group 93"
-                                    clip-path="url(#clip-path)"
-                                >
-                                    <path
-                                        id="Path_144"
-                                        data-name="Path 144"
-                                        d="M63.159,59.7A128.469,128.469,0,0,1,43.265,74.518a2.111,2.111,0,0,1-2.408,0,120.462,120.462,0,0,1-27.3-22.275A60,60,0,0,1,3.234,36.8,34.28,34.28,0,0,1,0,23.1a22.417,22.417,0,0,1,7.49-17.48A20.682,20.682,0,0,1,18.575.273,21.168,21.168,0,0,1,35.458,4.756a22.133,22.133,0,0,1,6.371,8.1c.059.125.129.246.16.3.942-1.493,1.769-3.046,2.817-4.421A21.245,21.245,0,0,1,59.131.226,21.969,21.969,0,0,1,83.446,16.99a24.859,24.859,0,0,1,.48,8.92A41.185,41.185,0,0,1,77.064,43.76,81.29,81.29,0,0,1,63.159,59.7"
-                                        transform="translate(0 0)"
-                                        fill="#fd084f"
-                                    />
-                                    <path
-                                        id="Path_145"
-                                        data-name="Path 145"
-                                        d="M83.446,16.991a24.86,24.86,0,0,1,.48,8.921A41.188,41.188,0,0,1,77.064,43.76,81.288,81.288,0,0,1,63.159,59.7,128.468,128.468,0,0,1,43.265,74.518a2.112,2.112,0,0,1-2.408,0,120.469,120.469,0,0,1-27.3-22.274A60,60,0,0,1,3.235,36.8,34.28,34.28,0,0,1,0,23.1a22.416,22.416,0,0,1,7.49-17.48A20.679,20.679,0,0,1,18.575.274,21.167,21.167,0,0,1,35.458,4.756a22.131,22.131,0,0,1,6.371,8.1c.059.125.129.246.16.3.942-1.493,1.769-3.046,2.817-4.421A21.244,21.244,0,0,1,59.131.227,21.969,21.969,0,0,1,83.446,16.991"
-                                        transform="translate(0 0)"
-                                        fill="#fd084f"
-                                    />
-                                    <path
-                                        id="Path_146"
-                                        data-name="Path 146"
-                                        d="M83.927,25.911A41.185,41.185,0,0,1,77.064,43.76,81.3,81.3,0,0,1,63.159,59.7,128.469,128.469,0,0,1,43.265,74.517a2.111,2.111,0,0,1-2.408,0,120.467,120.467,0,0,1-27.3-22.275A60,60,0,0,1,3.234,36.8,34.281,34.281,0,0,1,0,23.1a22.417,22.417,0,0,1,7.49-17.48A20.682,20.682,0,0,1,18.575.273,21.168,21.168,0,0,1,35.458,4.756a22.134,22.134,0,0,1,6.371,8.1c.059.125.129.246.16.3.942-1.493,1.769-3.046,2.817-4.421A21.246,21.246,0,0,1,59.131.226,21.969,21.969,0,0,1,83.446,16.99a24.856,24.856,0,0,1,.48,8.921"
-                                        transform="translate(0 0)"
-                                        fill="#fd084f"
-                                    />
-                                    <path
-                                        id="Path_147"
-                                        data-name="Path 147"
-                                        d="M83.927,25.911A41.185,41.185,0,0,1,77.064,43.76,81.3,81.3,0,0,1,63.159,59.7,128.469,128.469,0,0,1,43.265,74.517a2.111,2.111,0,0,1-2.408,0,120.467,120.467,0,0,1-27.3-22.275A60,60,0,0,1,3.234,36.8,34.281,34.281,0,0,1,0,23.1a22.417,22.417,0,0,1,7.49-17.48A20.682,20.682,0,0,1,18.575.273,21.168,21.168,0,0,1,35.458,4.756a22.134,22.134,0,0,1,6.371,8.1c.059.125.129.246.16.3.942-1.493,1.769-3.046,2.817-4.421A21.246,21.246,0,0,1,59.131.226,21.969,21.969,0,0,1,83.446,16.99a24.856,24.856,0,0,1,.48,8.921"
-                                        transform="translate(0 0)"
-                                        fill="#fd084f"
-                                    />
-                                </g>
-                            </svg>
-                        </h1>
-                    </div>
+                <div className="position-absolute top-0 start-0 py-3 px-5 d-flex gap-2 align-items-center fw-bolder text-white" style={{ backgroundImage: 'linear-gradient(90deg, #FD084F 0%, #3B46F2 100%)', borderBottomRightRadius: '1rem', fontSize: '5rem' }}>
+                    #TRUCARES
+                    <BsSuitHeart className="ms-3" />
                 </div>
-                <div className="bnnr_bot_box">
-                    <div className="bnnr_txt">
-                        <div className="h3">
-                            TRU AGENTS ARE MAKING A DIFFERENCE
-                        </div>
-                        <p>
-                            Our culture is defined by our values. At the core of
-                            those values is generosity. We believe in developing
-                            relationships with organizations and causes that
-                            positively impact their communities. Giving back is
-                            what we do, it’s who we are as an organization.
-                        </p>
-                    </div>
+                <div className="position-absolute bottom-0 end-0 w-75 bg-black bg-opacity-75 px-5 py-3 text-white" style={{ borderTopLeftRadius: '1rem' }}>
+                    <h1 className="fw-bold">
+                        TRU AGENTS ARE MAKING A DIFFERENCE
+                    </h1>
+                    <h4>
+                        Our culture is defined by our values. At the core of those values is generosity. We believe in developing relationships with organizations and causes that positively impact their communities. Giving back is what we do, it’s who we are as an organization.
+                    </h4>
                 </div>
             </div>
-            <GradientDivider />
+            <div className="w-100 py-3 mb-5" style={{backgroundImage:'linear-gradient(270deg, #3B46F2 0%, #FD084F 100%)'}}/>
         </>
     );
 }
--- src/pages/GivingBack/components/GivingComapanyLogo.tsx
+++ src/pages/GivingBack/components/GivingComapanyLogo.tsx
@@ -2,47 +2,17 @@ import React from "react";
 
 function GivingComapanyLogo() {
     return (
-        <div className="orgnztn_logo_sec cmmn_gap big_top_gap">
-            <div className="custom_container_fluid">
-                <div className="page_heading text-center">
-                    <h4>A few of the amazing organizations we have helped.</h4>
-                </div>
-                <div className="row align-items-center">
-                    <div className=" col-sm-3   orgntn_col">
-                        <div
-                            className="orgtn_lgo_box aos-init aos-animate"
-                            data-aos="zoom-in"
-                        >
-                            <img src="/img/orgntn_logo1.png" alt="" />
-                        </div>
-                    </div>
-                    <div className=" col-sm-3   orgntn_col">
-                        <div
-                            className="orgtn_lgo_box aos-init aos-animate"
-                            data-aos="zoom-in"
-                        >
-                            <img src="/img/orgntn_logo2.png" alt="" />
-                        </div>
-                    </div>
-                    <div className=" col-sm-3   orgntn_col">
-                        <div
-                            className="orgtn_lgo_box aos-init aos-animate"
-                            data-aos="zoom-in"
-                        >
-                            <img src="/img/orgntn_logo3.png" alt="" />
-                        </div>
-                    </div>
-                    <div className=" col-sm-3   orgntn_col">
-                        <div
-                            className="orgtn_lgo_box aos-init aos-animate"
-                            data-aos="zoom-in"
-                        >
-                            <img src="/img/orgntn_logo4.png" alt="" />
-                        </div>
-                    </div>
+            <div className="my-5 ">
+                <h2 className="fw-bold mb-5 text-center">
+                    AMAZING ORGANIZATIONS WE’VE HELPED
+                </h2>
+                <div className="d-flex align-items-center gap-5 justify-content-around px-5">
+                    <img src="/img/orgntn_logo1.png" alt="" />
+                    <img src="/img/orgntn_logo2.png" alt="" />
+                    <img src="/img/orgntn_logo3.png" alt="" />
+                    <img src="/img/orgntn_logo4.png" alt="" />
                 </div>
             </div>
-        </div>
     );
 }
 
